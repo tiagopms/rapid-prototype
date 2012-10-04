856e3b0dd3e69e360b7b9ebd8aa1d25ecb70d59b
João Felipe N. Pimentel <joaofelipenp@gmail.com>
2012-10-03 23:11:23 -0500

    Module3: lost password

--- module3/site/lost_password.php
+++ module3/site/lost_password.php
@@ -2,27 +2,9 @@
         $title = "Sign Up";
         $redirect = "home.php";
         $user_logged = false; 
-        include "header.php";
+        include "start.php";
+        require 'database.php';
+        $main_page = "lost_password_main.php";
+        include "base.php";
 ?>
-    <div class="lost_password_box">
-        <form action="check_lost_password.php" method="POST">
-            <span class="username">
-                <label for="username">Username:</label>
-                <input type="text" name="username" id="username"/>
-            </span>
-            <span class="email">
-                <label for="email">Email:</label>
-                <input type="text" name="email" id="email"/>
-            </span>
-            <span class="submit_button">
-                <input type="submit" name="signup" value="Submit"/>
-            </span>
-        </form>
-            <span class="cancel_button">
-                <a href="login.php">Cancel</a>
-            </span>
-    </div>
-</div>
-</body>
-</html>
-
+   
