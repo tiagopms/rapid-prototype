80b330ffd25f7c2efbd0b3f44d9c3da67260c852
João Felipe N. Pimentel <joaofelipenp@gmail.com>
2012-10-05 02:41:06 -0500

    Module3: refactoring admin

--- module3/site/check_delete_story.php
+++ module3/site/check_delete_story.php
@@ -30,20 +30,8 @@
             $stmt->fetch();
 
             $stmt->close();
-            $stmt = $mysqli->prepare("SELECT COUNT(*) FROM accounts WHERE id=? and admin='true'");
-            if(!$stmt){
-                printf("Query Prep 1 Failed: %s\n", $mysqli->error);
-                exit();
-            }
-            
-            $stmt->bind_param('s', $id);
-            $stmt->execute();
-            $stmt->bind_result($found2);
-            $stmt->fetch();
-
-            $stmt->close();
-            
-            if (!$found && !$found2) {
+        
+            if (!$found && !$admin) {
                 $_SESSION['error'] = 'You don\'t have permission dor that';
                 header('Location: home.php');
                 exit();
--- module3/site/check_edit_story.php
+++ module3/site/check_edit_story.php
@@ -34,20 +34,8 @@
             $stmt->fetch();
 
             $stmt->close();
-            $stmt = $mysqli->prepare("SELECT COUNT(*) FROM accounts WHERE id=? and admin='true'");
-            if(!$stmt){
-                printf("Query Prep 1 Failed: %s\n", $mysqli->error);
-                exit;
-            }
             
-            $stmt->bind_param('s', $id);
-            $stmt->execute();
-            $stmt->bind_result($found2);
-            $stmt->fetch();
-
-            $stmt->close();
-            
-            if (!$found && !$found2) {
+            if (!$found && !$admin) {
                 $_SESSION['error'] = 'You don\'t have permission for that';
                 header('Location: story.php?story='.$story_id);
                 exit();
