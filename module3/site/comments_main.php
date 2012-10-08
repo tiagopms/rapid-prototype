803e52db79c8a6683b55c98cf89d4acf4d5c9000
João Felipe N. Pimentel <joaofelipenp@gmail.com>
2012-10-08 12:12:53 -0500

    Module3: edit comment fix

--- module3/site/comments_main.php
+++ module3/site/comments_main.php
@@ -104,9 +104,9 @@
                     <?php echo nl2br(detect_links(process_text($text))); ?>
                 </p>
                 <?php
-                    $is_user = $commiter_id != $user;
-                    echo !$is_user;
-                    if ($admin || !$is_user) {
+                    $is_user = $commiter_id === $user;
+                    echo $is_user;
+                    if ($admin || $is_user) {
                         echo $admin;
                         echo $commiter_id;
                         echo $user;
