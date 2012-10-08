817947a56ab10f0a4b634a0347551225b0deeb37
João Felipe N. Pimentel <joaofelipenp@gmail.com>
2012-10-07 22:49:36 -0500

    Module3: admin fix

--- module3/site/message.php
+++ module3/site/message.php
@@ -1,10 +1,10 @@
 <?php
     if(isset($_SESSION['error']) && !empty($_SESSION['error'])) {
-        echo '<span class="error" id="'.htmlentities($title).'">'.htmlentities($_SESSION['error']).'</span>';
+        echo '<span class="error">'.htmlentities($_SESSION['error']).'</span>';
         unset($_SESSION['error']);
     }
     if(isset($_SESSION['success']) && !empty($_SESSION['success'])) {
-        echo '<span class="success" id="'.htmlentities($title).'">'.htmlentities($_SESSION['success']).'</span>';
+        echo '<span class="success">'.htmlentities($_SESSION['success']).'</span>';
         unset($_SESSION['success']);
     }
 ?>
